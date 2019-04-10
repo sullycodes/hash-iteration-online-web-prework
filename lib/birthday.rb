a6b1b4167cb86a9def322e4fb291f3a88303a1bc
Daniel Sullivan <dansullivan19@gmail.com>
2019-04-10 18:11:58 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,7 +6,8 @@ birthday_kids = {
 
 def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-    puts 
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
 end
 
 
